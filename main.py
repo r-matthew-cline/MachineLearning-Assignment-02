5688a61452713668ff0220194dda568100ccd68c
Matthew <cline.48@wright.edu>
2017-10-06 13:45:31 -0400

    Created main file and copied the data set into the root folder.

---
+++ main.py
@@ -0,0 +1,17 @@
+###############################################################################
+##
+## Logistic Regression
+##
+## @author: Matthew Cline
+## @version: 20171010
+##
+## Description: Logistic regression models mapping features of a flu survey
+##              to the risk of contracting flu. Uses single variable models
+##              as well as multivariable models. Also makes use of
+##              regularization and feature scaling.
+##
+###############################################################################
+
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
